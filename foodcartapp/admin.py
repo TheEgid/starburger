55dba994619cbfc2b8c5e5c531d74c401eb3c1ab
TheEgid <egid.f1@gmail.com>
2021-03-27 13:31:50 +0300

    Add changed order redirect

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
-
+from django.shortcuts import redirect
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.utils.encoding import iri_to_uri
 from .models import Product
 from .models import ProductCategory
 from .models import Restaurant
@@ -129,6 +131,14 @@ class OrderAdmin(admin.ModelAdmin):
     search_fields = [
         'phonenumber',
     ]
+
+    def response_change(self, request, obj):
+        res = super(OrderAdmin, self).response_change(request, obj)
+        if "next" in request.GET:
+            if url_has_allowed_host_and_scheme(request.GET['next'], None):
+                return redirect(iri_to_uri(request.GET['next']))
+        else:
+            return res
 
 
 @admin.register(OrderItem)
